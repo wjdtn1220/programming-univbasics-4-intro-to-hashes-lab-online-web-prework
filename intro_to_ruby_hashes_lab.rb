190d14551ac4ee49a50a4871bead5a98031a5132
Jeong Park <wjdtn1220@gmail.com>
2019-07-31 23:39:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,8 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  hash = {key: "value"}
+  hash[:key] = "value"
+  return hash
   
 end
 
