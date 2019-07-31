b1a4b97700c2e74032f0ba01e5ce13fe88b511de
Jeong Park <wjdtn1220@gmail.com>
2019-07-31 21:12:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,14 @@
 def new_hash
-  # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {name: "James"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {name: "Grace Hopper"}
 end
 
 def id_generator
