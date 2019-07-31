630fbb906f89f4c2617cebe30d01a36a7533587a
Jeong Park <wjdtn1220@gmail.com>
2019-07-31 22:07:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-    read_from_hash[:name] = "hash"
+    read_from_hash{name: "Steve"} = 
 end
 
 def update_counting_hash(hash, key)
