326cbddc4bd50a13169f01300615447eef7e54c8
Jeong Park <wjdtn1220@gmail.com>
2019-07-31 22:11:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,6 +21,8 @@ def my_hash_creator(key, value)
   my_hash_creator{key: "value"}
   my_hash_creator[:key]
   my_hash_creator[:key] = "value"
+  my_hash_creator[:key]
+  my_hash_creator
 end
 
 def read_from_hash(hash, key)
