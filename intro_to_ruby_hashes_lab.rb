89f4a1c955d59f16afbe6408588a6866c5147c90
Jeong Park <wjdtn1220@gmail.com>
2019-07-31 21:28:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
-  id_generator = {apple: 7}
+  id_generator = {:id => 7.00}
 end
 
 def my_hash_creator(key, value)
