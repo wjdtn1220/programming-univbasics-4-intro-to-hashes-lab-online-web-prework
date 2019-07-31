927b490a998988183b69d18cd07d03c600aa3f57
Jeong Park <wjdtn1220@gmail.com>
2019-07-31 23:50:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  hash[:new_key] = "New Value"
+  hash[
 end
 
 def read_from_hash(hash, key)
